c7d8305a72c701a106d72c78753434bb3bc69149
ThePatrickCastle <patrickcastle0507@gmail.com>
2024-11-04 17:28:26 -0600

    Adding Files

---
+++ emacs/init.el
@@ -0,0 +1,19 @@
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("de4382be014e101c135c3d7c638a488e5cb843fa61554008f08e7bec1bc1abb6" "4861ed05311cce238097dc81a922d28c291c3073512d2a2d9a36dc3855dadc5c" "1b786d3723357a41cbb42c0a9f7e000e3f8c1c488691c0e92b7c1c69d49bf169" "4c33bd6d84f2155c63aca2f49b6564f616030e32bddef1cf41e34ff70a4f3eb2" "9d8d60e69db09062e759f008e59fac1d76618b09cddaf06cef093653c16f1e75" "6ed2a90b13f77bac6b75f36352d72378b2a17346518884675aa56339a3bc38a9" default))
+ '(display-line-numbers-type 'relative))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
